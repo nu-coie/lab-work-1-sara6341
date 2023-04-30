aeaffbf3ce33b0dc547e37870b6236f949eec725
sara6341 <130349386+sara6341@users.noreply.github.com>
2023-04-30 03:20:08 +0000

    ddd, Apr 4/30/2023, YYYY, 3:20 A +0

--- calculator.c
+++ calculator.c
@@ -7,7 +7,7 @@ int main(void)
 // Accept Input From user (char), where (a) stands for "Addition", (m) for "Multiplications," and (d) for "Division,"
   int num1 = 3;
   int num2 = 6;
-  printf("%d \n, num1 ")
+  printf("%d \n, num1 + num2")
 
 
 
